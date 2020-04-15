2fa168c087a3fac0e6882717b0e7d052748e99c5
vn0sygm <srinivas.makkena@samsclub.com>
2020-04-14 21:10:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/customexpandablelistviewcheckboxradiobutton/EcommerceFIltersExpandableAdapter.java
@@ -0,0 +1,204 @@
+package com.example.customexpandablelistviewcheckboxradiobutton;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class EcommerceFIltersExpandableAdapter extends BaseExpandableListAdapter {
+
+    private Activity _context;
+    private List<FilterHeader> _listDataHeader;
+    private HashMap<String, List<String>> _listDataChild;
+    int selectedPosition = 0;
+
+    HashMap<Integer, Integer> childCheckedState = new HashMap<>();
+    HashMap<Integer, Integer> childCheckboxState = new HashMap<>();
+    ArrayList<String> listOfStatusFilters = new ArrayList<>();
+
+    ArrayList<ArrayList<Integer>> check_states = new ArrayList<ArrayList<Integer>>();
+
+    public EcommerceFIltersExpandableAdapter(Activity _context, List<FilterHeader> _listDataHeader, HashMap<String, List<String>> _listDataChild) {
+        this._context = _context;
+        this._listDataHeader = _listDataHeader;
+        this._listDataChild = _listDataChild;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this._listDataHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition).getTitle())
+                .size();
+    }
+
+    @Override
+    public FilterHeader getGroup(int groupPosition) {
+        return this._listDataHeader.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition).getTitle())
+                .get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @SuppressLint("InflateParams")
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition).getTitle();
+        //if (convertView == null) {
+        LayoutInflater infalInflater = (LayoutInflater) this._context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = Objects.requireNonNull(infalInflater).inflate(R.layout.filter_header_layout, null);
+        //}
+
+        TextView lblListHeader = convertView.findViewById(R.id.lblListHeader);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+        TextView groupStatus = convertView.findViewById(R.id.groupStatus);
+
+        groupStatus.setText(_listDataHeader.get(groupPosition).getActiveFilter());
+
+
+        return convertView;
+    }
+
+    @SuppressLint("InflateParams")
+    @Override
+    public View getChildView(final int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final String childText = (String) getChild(groupPosition, childPosition);
+        final FilterHeader headerText = (FilterHeader) getGroup(groupPosition);
+        //if (convertView == null) {
+        LayoutInflater infalInflater = (LayoutInflater) this._context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = Objects.requireNonNull(infalInflater).inflate(R.layout.filter_child_layout, null);
+        // }
+
+        final TextView filterName = convertView
+                .findViewById(R.id.textviewFilterName);
+        filterName.setText(childText);
+        final CheckBox filterCheckBox = convertView.findViewById(R.id.filterNameCheckBox);
+        final RadioButton filterRadioButton = convertView.findViewById(R.id.filterNameRadioButton);
+        //txtListChild.setText(childText);
+        if (groupPosition > 0) {
+            filterCheckBox.setVisibility(View.GONE);
+            filterRadioButton.setVisibility(View.VISIBLE);
+        } else {
+            filterCheckBox.setVisibility(View.VISIBLE);
+            filterRadioButton.setVisibility(View.GONE);
+        }
+
+
+
+
+        View view = parent.getChildAt(groupPosition);
+
+        try {
+
+
+            try {
+                if (childCheckedState != null)
+                    if (childCheckedState.size() > 0)
+                        filterRadioButton.setChecked(childPosition == childCheckedState.get(groupPosition));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            filterRadioButton.setTag(childPosition);
+            filterRadioButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    childCheckedState.put(groupPosition, (Integer) v.getTag());
+                    if (_listDataChild.get(headerText.getTitle()) != null) {
+                        headerText.setActiveFilter(_listDataChild.get(headerText.getTitle()).get(childPosition));
+
+                    }
+                    notifyDataSetChanged();
+
+
+                }
+            });
+
+            if (childCheckboxState.size() > 0) {
+                if (childCheckboxState.get(childPosition) != null) {
+                    if (childCheckboxState.get(childPosition) == 0) {
+                        filterCheckBox.setChecked(false);
+                    } else {
+                        filterCheckBox.setChecked(true);
+                    }
+                }
+            }
+
+            filterCheckBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (filterCheckBox.isChecked()) {
+                        childCheckboxState.put(childPosition, 1);
+                        listOfStatusFilters.add(_listDataChild.get(headerText.getTitle()).get(childPosition));
+                    } else {
+                        childCheckboxState.put(childPosition, 0);
+                        listOfStatusFilters.remove(_listDataChild.get(headerText.getTitle()).get(childPosition));
+                    }
+
+                    if (_listDataChild.get(headerText.getTitle()) != null)
+                        headerText.setActiveFilter(getCheckedStatusCombinedString());
+                    notifyDataSetChanged();
+
+                }
+            });
+
+        } catch (Exception e) {
+
+        }
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+
+    public String getCheckedStatusCombinedString() {
+        String status = "";
+        for (int i = 0; i < listOfStatusFilters.size(); i++) {
+            status += listOfStatusFilters.get(i);
+            if (i != listOfStatusFilters.size() - 1) {
+                status += ",";
+            }
+        }
+        return status;
+    }
+}
---
+++ app/src/main/java/com/example/customexpandablelistviewcheckboxradiobutton/EcommerceFiltersFragment.java
@@ -0,0 +1,163 @@
+package com.example.customexpandablelistviewcheckboxradiobutton;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import butterknife.internal.DebouncingOnClickListener;
+
+
+public class EcommerceFiltersFragment extends Fragment {
+
+
+    LinearLayout linearLayout;
+
+    Button applyFiltersButton;
+
+    TextView textviewReset;
+
+    ExpandableListView expandablelistviewFilter;
+
+    MainActivity mContext;
+    List<FilterHeader> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+
+    EcommerceFIltersExpandableAdapter listAdapter;
+
+    private OnFragmentInteractionListener mListener;
+
+    public EcommerceFiltersFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static EcommerceFiltersFragment newInstance() {
+        EcommerceFiltersFragment fragment = new EcommerceFiltersFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        expandablelistviewFilter = view.findViewById(R.id.expandablelistview_filter);
+        linearLayout = view.findViewById(R.id.linearlayout_filter);
+        applyFiltersButton = view.findViewById(R.id.applyFiltersBtn);
+        textviewReset = view.findViewById(R.id.button_reset);
+        prepareListData();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_filter, container, false);
+        return view;
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<FilterHeader>();
+        listDataChild = new HashMap<>();
+        // Adding child data
+        listDataHeader.add(new FilterHeader("Item Size", "All sizes"));
+        listDataHeader.add(new FilterHeader("Item Color", "Select"));
+        listDataHeader.add(new FilterHeader("Sort", "Select"));
+
+
+// Adding child data
+        List<String> byDate = new ArrayList<>();
+        byDate.add("Medium");
+        byDate.add("Small");
+        byDate.add("Large");
+        byDate.add("XSmall");
+        byDate.add("XLarge");
+
+
+        List<String> category = new ArrayList<>();
+        category.add("Black");
+        category.add("Red");
+        category.add("White");
+        category.add("Green");
+        category.add("Blue");
+
+        List<String> sort = new ArrayList<>();
+        sort.add("Cost ($): highest first");
+        sort.add("Cost ($): lowest first");
+
+
+        listDataChild.put(listDataHeader.get(0).getTitle(), byDate); // Header, Child data
+        listDataChild.put(listDataHeader.get(1).getTitle(), category);
+        listDataChild.put(listDataHeader.get(2).getTitle(), sort);
+
+        listAdapter = new EcommerceFIltersExpandableAdapter(mContext, listDataHeader, listDataChild);
+        expandablelistviewFilter.setAdapter(listAdapter);
+
+        linearLayout.setOnClickListener(new DebouncingOnClickListener() {
+            @Override
+            public void doClick(View v) {
+                mContext.dismissFragment();
+            }
+        });
+
+        applyFiltersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mContext.dismissFragment();
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof MainActivity) {
+            mContext = (MainActivity) context;
+
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        // : Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
